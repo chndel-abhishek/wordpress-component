463f82c51f56cb5b0a57bfb63f6856e166c72059
Abhishek Chandel <abhishekchandel75900@gmai.com>
2024-05-15 15:22:42 +0530

    Wordpress added

---
+++ plugins/my-plugin/my-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: My Custom Plugin
+Description: Custom functionality for the WordPress site.
+Version: 1.0
+Author: Your Name
+*/
+
+// Your plugin code here
---
+++ themes/my-theme/index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+<div id="content">
+    <h1>Hello from WordPress!</h1>
+</div>
+<?php get_footer(); ?>
