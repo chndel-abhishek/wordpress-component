92e5257746a425950fa81aef791749480bd5b0d0
Abhishek Chandel <abhishekchandel75900@gmai.com>
2024-05-15 16:19:48 +0530

    Wordpress CI added

--- themes/my-theme/index.php
+++ themes/my-theme/index.php
@@ -1,9 +1,12 @@
-<?php
-get_header(); // Load the header of the theme
-?>
+                    
+<?php get_header(); ?>
 <div id="content">
-    <h1>Hello from WordPress!</h1> <!-- Display a heading -->
+    <h1>Hello from WordPress!</h1>
 </div>
-<?php
-get_footer(); // Load the footer of the theme
-?>
+<?php get_footer(); ?>
+                    
+                
+
+
+
+
