71d664699f6aa8050a733ec1d7bdc98c8c040b21
Abhishek Chandel <abhishekchandel75900@gmail.com>
2024-05-15 19:52:12 +0530

    Added the go component

--- themes/my-theme/index.php
+++ themes/my-theme/index.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * File: index.php
+ * Description: Example file for Developer News theme.
+ * Author: Your Name
+ * Version: 1.0.0
+ */
+
 echo 'Hello!<br>';
 echo 'Welcome to Developer News<br>';
 echo 'Enjoy all of the ad-free articles<br>';
