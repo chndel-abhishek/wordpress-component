ed08e8bf2baca357929c8cae20a0fdc03eb51724
Abhishek Chandel <abhishekchandel75900@gmail.com>
2024-05-15 20:16:07 +0530

    Added the go component

--- themes/my-theme/index.php
+++ themes/my-theme/index.php
@@ -1,12 +1,3 @@
 <?php
-/**
- * File: example.php
- * Description: A simple PHP file without any errors.
- * Author: John Doe
- * Version: 1.0.0
- * @package ExamplePackage
- */
-
-// Print a simple message
-echo 'Hello, World!';
+echo "Abhishek " ."\n"."Wordpress";
 ?>
